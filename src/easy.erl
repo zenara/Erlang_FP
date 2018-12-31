85cca1f1d56f9ae6ae06a4cb8929ae05fab8d472
zenara <zenaronline@gmail.com>
2018-12-31 10:14:25 +0530

    case statement

--- src/easy.erl
+++ src/easy.erl
@@ -1,6 +1,6 @@
 -module(easy).
 
--export([add/2, factorial/1]).
+-export([add/2, factorial/1, area/3]).
 
 add(X, Y) ->		%% Head
 	X+Y.			%% Body
@@ -9,3 +9,10 @@ factorial(N) when N > 0 ->
 	N * factorial(N-1);
 factorial(0) ->
 	1.
+
+area(Type, N, M) ->
+	case Type of
+		square -> N*N;
+		circle -> math:pi() * N * N;
+		triangle -> 0.5 * N * M
+	end.
