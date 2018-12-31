10e826e750506be9be98cb5079cf55e281ac2202
zenara <zenaronline@gmail.com>
2018-12-31 13:20:54 +0530

    Basic Concurrency

--- src/easy.erl
+++ src/easy.erl
@@ -1,6 +1,6 @@
 -module(easy).
 
--export([add/2, factorial/1, factorial/2, area/3, area/1, speak/1]).
+-export([add/2, factorial/1, factorial/2, area/3, area/1, speak/1, start_concurrency/2, say_something/2]).
 
 add(X, Y) ->		%% Head
 	X+Y.			%% Body
@@ -31,8 +31,8 @@ speak(Animal) ->
 			   (Animal == cat) -> meaw;
 			   (Animal == dog) -> woofwoof;
 			   true -> no_animal
-		   end.
-%% ,	io.format("~w Says ~w ~n",[Animal,Talk]).
+		   end,
+	io:format("~w Says ~w ~n",[Animal,Talk]).
 
 
 %% accumulators
@@ -40,3 +40,13 @@ factorial(N, TotalFactorial) when N > 0 ->
 	factorial(N -1, N * TotalFactorial);
 factorial(0, TotalFactorial) ->
 	TotalFactorial.
+
+say_something(_,0) ->
+	io:format("Done ~n");
+say_something(Value,Times) ->
+	io:format("~s ~n",[Value]),
+	say_something(Value, Times-1).
+
+start_concurrency(Value1, Value2) ->
+	spawn(easy,say_something,[Value1,3]),
+	spawn(easy,say_something,[Value2,3]).
